640c3d2f15d265cf952166d05b6680de6ef78024
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-23 13:13:37 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,8 @@
+
 def alphabetize(arr)
-  # code here
-  sort = arr.sort { |a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z| a<=>z }
-  return sort
+  arr.sort_by do |sentence|
+      sentence.split("").map do |character|
+        ESPERANTO_ALPHABET.index(character)
+      end
+    end
 end
